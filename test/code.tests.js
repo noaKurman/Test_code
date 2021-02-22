7a3d01f479f10f6936a3982a5f46d2d2da991f05
Noa Kurman <kurman.noa@gmail.com>
2021-02-22 16:43:04 +0200

    test

--- test/code.tests.js
+++ test/code.tests.js
@@ -1,7 +1,7 @@
 import assert from 'assert'
 import { code } from '../code.js'
 
-// describe('code', function () {
+describe('code', function () {
 //   it('should return hi for hi user', () => {
 //     assert(code('hi'))
 //   })
