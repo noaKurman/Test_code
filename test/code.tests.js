17ffbf6765b4412b539259a6a4e8917542c693d7
Noa Kurman <kurman.noa@gmail.com>
2021-02-22 14:46:48 +0200

    remove test

--- test/code.tests.js
+++ test/code.tests.js
@@ -1,10 +1,10 @@
 import assert from 'assert'
 import { code } from '../code.js'
 
-describe('code', function () {
-  it('should return hi for hi user', () => {
-    assert(code('hi'))
-  })
+// describe('code', function () {
+//   it('should return hi for hi user', () => {
+//     assert(code('hi'))
+//   })
 
   it('should return err for no user', () => {
     assert(!code())
